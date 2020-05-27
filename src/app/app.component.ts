1f3334347df05b15b8ecf1b08266e7bb4539941e
Emily Vong <emily.vong25@gmail.com>
2020-05-27 08:12:01 -0500

    url shortening working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,56 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'url-shortening-landing-page';
+  urlToShorten = '';
+  
+  
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+
+  }
+
+  submitUrl() {
+    if(!this.urlToShorten || this.urlToShorten.length < 1) {
+      alert('Enter a valid url to shorten');
+      return;
+    }
+    console.log(this.urlToShorten);
+
+    this.newShortUrl();
+  }
+
+  newShortUrl() {
+    let urlObj = {};
+    urlObj['url'] = this.urlToShorten;
+    this.postUrlRequest(urlObj)
+      .subscribe(
+        (resp) => {
+          console.log('resp', resp)
+        },
+        (err) => {
+          console.log('err', err)
+        }
+      );
+  }
+
+
+  // -- Service methods - may create service component
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+
+  postUrlRequest(url): Observable<any> {
+    return this.http.post('https://rel.ink/api/links/', JSON.stringify(url), this.httpOptions);
+  }
+
+  getUrlInfoRequest(hash: string): Observable<any> {
+    return this.http.get('https://rel.ink/api/links/' + hash);
+  }
+
+
 }
